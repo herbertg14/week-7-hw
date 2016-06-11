2c0e3c4ec2142a17657c9b38a942c9c2dbdc77a0
herbertg14 <herbert.gutierrez@utexas.edu>
2016-06-11 18:16:29 -0500

    got page to refresh

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,11 @@
 
+// access firebase
 var trainData = new Firebase("https://hw-7.firebaseio.com/");
 
+// when add a train button is clicked 
 $("#addTrainButton").on("click",function(){
 
+	//get info from form
 	var trainName = $("#trainNameInput").val().trim();
 	var destination = $("#destinationInput").val().trim();
 
@@ -11,6 +14,7 @@ $("#addTrainButton").on("click",function(){
 
 	var frequency = parseInt($("#frequencyInput").val().trim());
 
+	//create object
 	var newTrain = {
 		trainName: trainName,
 		destination: destination,
@@ -18,18 +22,48 @@ $("#addTrainButton").on("click",function(){
 		start: firstTrainTime
 	}
 
+	//add object to firebase
 	trainData.push(newTrain);
 
-
+	// reset values on form
 	$("#trainNameInput").val("");
 	$("#destinationInput").val("");
 	$("#firstTimeInput").val("");
 	$("#frequencyInput").val("");
 
+
+	//prevent the page from refreshing
 	return false;
 });
 
-function everyTime(childSnapshot, prevChildKey){
+//adds data to table on page from firebase when page is loaded
+function loadPage(childSnapshot, prevChildKey){
+
+	var key = childSnapshot.key();
+	var trainName = childSnapshot.val().trainName;
+	var destination = childSnapshot.val().destination;
+	var frequency = childSnapshot.val().frequency;
+	var start = childSnapshot.val().start;
+
+	var firstMoment = moment(start, "HH:mm").subtract(1,"years");
+
+	var differenceTime = moment().diff(moment(firstMoment), "minutes");
+
+	var timeRemaining = differenceTime % frequency;
+
+	var timeOfNext = frequency - timeRemaining;
+
+	var nextTrainTime = moment().add(timeOfNext,"minutes");
+
+    $("#scheduleTable > tbody").append("<tr id=" + key + "><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + moment(nextTrainTime).format("LT") + "</td><td>" + timeOfNext + "</td></tr>");
+}
+
+//load data on page
+trainData.on("child_added", loadPage);
+
+//refeshes data on the table from firebase
+function refresh(childSnapshot, prevChildKey){
+	var key = childSnapshot.key();
 
 	var trainName = childSnapshot.val().trainName;
 	var destination = childSnapshot.val().destination;
@@ -46,19 +80,15 @@ function everyTime(childSnapshot, prevChildKey){
 
 	var nextTrainTime = moment().add(timeOfNext,"minutes");
 
-    $("#scheduleTable > tbody").append("<tr id=" + trainName + "><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + moment(nextTrainTime).format("LT") + "</td><td>" + timeOfNext + "</td></tr>");
+    $("#" + key).html("<td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + moment(nextTrainTime).format("LT") + "</td><td>" + timeOfNext + "</td></tr>");
 }
 
-trainData.on("child_added", everyTime);
+//calls on refresh function
+function autoRefresh() {
+	trainData.on("child_added", refresh);
+}
 
-// function refresh(){}
-
-
-// function autoRefresh() {
-// 	trainData.on("child_added", refresh);
-// }
- 
-
-// setInterval('autoRefresh()', 60000);
+//time interval for refreshing page
+setInterval('autoRefresh()', 60000);
 
 
